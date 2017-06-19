fa639afb6fcf3fe5001296352910f59bbae27d4a
Tutu George <tutugeorge@gmail.com>
2017-06-19 22:25:11 +0530

    Adding intial set of files

---
+++ ECSPrinterCommands/Command.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ECSPrint
+{
+    public static class Command
+    {
+        public static char NewLine = (char)10;        
+        public static string LeftJustify = (char)27 + "a0";
+        public static string CenterJustify = (char)27 + "a1";
+        public static string RightJustify = (char)27 + "a2";
+        public static string Bold = (char)27 + "E1";
+        public static string Unbold = (char)27 + "E0";
+    }
+}
